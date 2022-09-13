aa0444add43ae49068d7e8fe7d7ed1597410a43a
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2022-09-13 09:14:15 +0600

    signup and forgot password created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import ForgotPassword from './components/Login/ForgotPassword';
+import SignUp from './components/Login/SignUp';
+import News from './components/News/News';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='forgotpassword'element={<ForgotPassword/>}/>
+        <Route path='/news' element={<News/>}/>
       </Routes>
     </div>
   );
